f4b6f582835b6f5de95a6092afadf0732b18d2dc
splinter <85017105+Splinter047@users.noreply.github.com>
2024-11-30 17:16:29 +0500

    Imported SFML

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,21 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
-int main() {
+int main()
+{
+	auto window = sf::RenderWindow({800u, 800u}, "Reversi");
+	window.setFramerateLimit(60);
 
+	while (window.isOpen())
+	{
+		for (auto event = sf::Event(); window.pollEvent(event);)
+		{
+			if (event.type == sf::Event::Closed)
+			{
+				window.close();
+			}
+		}
+
+		window.clear();
+		window.display();
+	}
 }
